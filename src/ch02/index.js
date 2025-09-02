5c8495d217e2e532839f4a7f36614454dc5ce333
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-09-02 11:10:35 +0900

    ch02 변수와 상수

---
+++ src/ch02/index.js
@@ -0,0 +1,41 @@
+// 변수 할당
+// let name;
+// name = "은현;
+
+// console.log(name);
+
+// 변수 재할당
+// let name = "은현";
+// name = "cute";
+
+// console.log(name);
+
+// 변수 명명 규칙
+// let $name;
+// let my_name;
+
+// let name^;
+// let my*name;
+
+// let 1st;
+// let user1;
+// let te123st;
+
+// let var;
+// let if;
+
+// 긴 변수명 표기법 - 카멜 표기법
+// let userInfo;
+// let isTrueOrFalse;
+
+// 상수
+// const name = "은현";
+// name = "cute";
+
+// console.log(name);
+
+// const birthday = "03-15";
+// const name = "eunhyeon";
+
+// const EMAIL_ADDRESS = "kaeuhy_0315@naver.com";
+// const PROFILE_URL = "https://kaeuhy.com";
